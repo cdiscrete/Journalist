48a03239c36f626fcd0a3c2b42dcd8f18d655f95
Discrete Chan <c.discrete@gmail.com>
2014-02-02 18:36:20 +0800

    Edit author information and remove deadlink.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div>
 
 <div id="footer">
-	<p>The Journalist template by <a href="http://lucianmarin.com/" rel="designer">Lucian E. Marin</a> - enhanced by <a href="http://yoschi.cc/">Roger Sikorski</a> &mdash; Built for <a href="http://wordpress.org/">WordPress</a></p>
+	<p>The Journalist template by <a href="http://lucianmarin.com/" rel="designer">Lucian E. Marin</a> - enhanced by <a href="https://github.com/RogerSik">Roger Sikorski</a> and <a href="https://github.com/cdiscrete/Journalist/">Discrete Chen</a> &mdash; Built for <a href="http://wordpress.org/">WordPress</a></p>
 </div>
 <?php do_action('wp_footer', ''); ?>
 
